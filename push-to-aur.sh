8ad59ebbc924a23674afc7a0de998008713d2344
Grey Christoforo <grey@christoforo.net>
2016-03-10 14:55:51 +0000

    Create push-to-aur.sh

---
+++ push-to-aur.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# this script syncs the branches in this repo to their counterpart AUR package repos
+
+BRANCHES=$(git branch --column)
+BRANCHES=${BRANCHES/\*/}
+BRANCHES=${BRANCHES/master/}
+BRANCHES=($BRANCHES)
+
+for i in ${BRANCHES[@]}; do
+        git remote add ${i}-AUR ssh+git://aur@aur.archlinux.org/${i}.git
+        git checkout ${i}
+        updpkgsums
+        mksrcinfo
+        sed -i '2d' .SRCINFO
+        git add PKGBUILD
+        git add .SRCINFO
+        git commit -m "update checksums and srcinfo"
+        git push ${i}-AUR +${i}:master
+        git remote remove ${i}-AUR
+done
